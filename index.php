87a8ae5562ae773909c8619e6f89bf1891a8ffe6
Martin <martin.knudsen.sand@gmail.com>
2019-02-21 07:16:46 +0100

    innlogget side etter bruker har logget inn
    Lagg til riktig tema og lagg utloggingsknappen inn i navbar

--- index.php
+++ index.php
@@ -15,14 +15,33 @@
 <html>
 <head>
 	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
 </head>
-<body>
+<body style="background-color:rgb(29, 28, 28)">
+    <nav class="navbar navbar-expand-lg navbar-light navbar-dark" style="background-color: #383838; color:white;">
+        <a class="navbar-brand" href="index.html"><img src="img/FerdigLogo.png" alt="logo" style="height:100px;"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-<div class="header">
-	<h2>Home Page</h2>
-</div>
-<div class="content">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="use.html">Hjelp</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.html">Om oss</a>
+                </li>
+								<li class="nav-item">
+									<a class="nav-link" href="index.php?logout='1'" style="color: red;">Logg ut</a>
+								</li>
+            </ul>
+
+        </div>
+    </nav>
+<div class="content" style="color:white;">
   	<!-- notification message -->
   	<?php if (isset($_SESSION['success'])) : ?>
       <div class="error success" >
@@ -38,7 +57,6 @@
     <!-- logged in user information -->
     <?php  if (isset($_SESSION['username'])) : ?>
     	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
-    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
     <?php endif ?>
 </div>
 		
