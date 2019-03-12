c4d1ac65836cf663d3952602229aba6f95456448
tare <tba.97@hotmail.com>
2019-03-12 19:24:07 +0100

    Byttet til landingpage.php

--- use.php
+++ use.php
@@ -14,7 +14,7 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light navbar-dark" id="navbar">
-        <a class="navbar-brand" href="index.php"><img src="img/FerdigLogo.png" alt="logo" style="height:100px;"></a>
+        <a class="navbar-brand" href="landingpage.php"><img src="img/FerdigLogo.png" alt="logo" style="height:100px;"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
