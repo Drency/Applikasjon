5b81e465046845777a8c96b456e8667ed6f5c9fd
tare <tba.97@hotmail.com>
2019-03-13 18:30:32 +0100

    Fikset på tekst about

--- about.php
+++ about.php
@@ -9,21 +9,22 @@
 <div class="card-deck" id="bg-color">
     <div class="card" id="bg-color">
         <h2 class="card-title">Studenter</h2>
-        <pre class="card-text">
-        New Home ble lagd av 4 studenter i USN.
-        Vi bestemte oss å lage applikasjonen fordi vi likte å ha et system på linker, 
-        og vi synes bokmerke på nettleseren ikke var godt nok
-        Første halvåret på dette porsjekte, ble å finne ut ideer,
-        og hvordan vi skulle fullføre oppgaven vår.
-        Siste halvåret har blitt å gjøre oppgaven vår ferdig.</pre>
+        <pre class="card-text" id="about-heading">
+            New Home ble lagd av 4 studenter i USN.
+            Vi bestemte oss å lage applikasjonen fordi vi likte å ha et system på linker,
+            og vi synes bokmerke på nettleseren ikke var godt nok
+            Første halvåret på dette porsjekte, ble å finne ut ideer,
+            og hvordan vi skulle fullføre oppgaven vår.
+            Siste halvåret har blitt å gjøre oppgaven vår ferdig.</pre>
         </div>
         <div class="card" id="bg-color">
             <h2 class="card-title">New Home</h2>
-            <p class="card-text">New Home er et sted for deg, som har mange linker og</p>
-            <p>liker å he et system på ting.</p>
-            <p>Vi er her for å gjøre hverdagen din litt lettere.</p>
-            <p>Du slipper å leite eller miste linkene dine, som du kanskje trenger senere.</p>
-            <p>Med en litt enklere hverddag blir livet litt bedre.</p>
+            <pre class="card-text" id="about-heading">
+            New Home er et sted for deg, som har mange linker og
+            liker å he et system på ting.
+            Vi er her for å gjøre hverdagen din litt lettere.
+            Du slipper å leite eller miste linkene dine, som du kanskje trenger senere.
+            Med en litt enklere hverddag blir livet litt bedre.</pre>
         </div>
     </div>
 </div>
