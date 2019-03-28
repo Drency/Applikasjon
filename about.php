f9df1c925c7ff8fbcf8574cbe1cf2b7f2e5b1369
tare <tba.97@hotmail.com>
2019-03-28 16:43:59 +0100

    Fjernet fra css. Adda til about
    Forandret tekst, og style

--- about.php
+++ about.php
@@ -7,32 +7,32 @@
 ?>
 
 <!-- Cards for landing page -->
-<h1 class="text-center" id="about-heading">Om Oss</h1>
+<h1 class="text-center" id="index-heading">Om Oss</h1>
 
-    
-<div class="card-deck" id="bg-color">
-    <div class="card" id="bg-color">
-        <h2 class="card-title">Studenter</h2>
-        <pre class="card-text" id="about-heading">
-            <p>New Home ble lagd av 4 studenter i USN.</p>
-            Vi bestemte oss å lage applikasjonen fordi vi likte å ha et system på linker,
-            og vi synes bokmerke på nettleseren ikke var godt nok
-            Første halvåret på dette porsjekte, ble å finne ut ideer,
-            og hvordan vi skulle fullføre oppgaven vår.
-            Siste halvåret har blitt å gjøre oppgaven vår ferdig.</pre>
-        </div>
-        <div class="card" id="bg-color">
-            <h2 class="card-title">New Home</h2>
-            <pre class="card-text" id="about-heading">
-            New Home er et sted for deg, som har mange linker og
-            liker å he et system på ting.
-            Vi er her for å gjøre hverdagen din litt lettere.
-            Du slipper å leite eller miste linkene dine, som du kanskje trenger senere.
-            Med en litt enklere hverddag blir livet litt bedre.</pre>
+<div class="container">
+    <div class="text-center">
+        <div class="card-deck" id="bg-color">
+            <div class="card" id="bg-color">
+                <h2 class="card-title">Studenter</h5>
+                <p class="card-text">New Home ble lagd av 4 studenter i USN.
+                Vi bestemte oss å lage applikasjonen fordi vi liker å ha et system på linker.
+                Vi synes bokmerker på nettleseren ikke var godt nok.
+                Første halvåret på dette porsjektet ble å finne ut ideer
+                og hvordan vi skulle fullføre oppgaven vår.
+                Siste halvåret har blitt å gjøre oppgaven vår ferdig.</p>
+            </div>
+                <div class="card" id="bg-color">
+                <h2 class="card-title">New Home</h5>
+                <p class="card-text">New Home er et sted for deg som har mange linker, og
+                liker å ha et system på det du trenger.
+                Vi er her for å gjøre hverdagen din litt lettere.
+                Du slipper å leite eller miste linkene dine, som du kanskje trenger senere.
+                Med en litt enklere hverddag blir livet litt bedre.</p>
+                
+            </div>
         </div>
     </div>
 </div>
-
 
 <?php
     require_once __DIR__ . '/include/footer.php';
