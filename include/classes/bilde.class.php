34b70f551f2ff62fb78bc67d7bd9899af1ab9918
tare <tba.97@hotmail.com>
2019-03-28 20:46:51 +0100

    Bilde class
    Ikke fullført, men skal være class for bilder

---
+++ include/classes/bilde.class.php
@@ -0,0 +1,19 @@
+<?php
+    
+
+class Bilde
+{
+    private $id
+    private $bildelink
+
+    public function _construct($id = 0, $bildelink = "")
+    {
+        $this->id = $id;
+        $this->bildelink = $bildelink;
+    }
+
+    
+
+
+
+}
