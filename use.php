751f37e192053fddca73fff4fb98e6d16e85a604
tare <tba.97@hotmail.com>
2019-04-07 23:00:24 +0200

    adda til på use.php

--- use.php
+++ use.php
@@ -6,6 +6,102 @@
 ?>
 
 
+<!-- Cards for use page -->
+<h1 class="text-center" id="use-heading">Hvordan bruke applikasjonen</h1>
+
+<div class="container text-center ">
+    <div class="col-sm-4">
+        <div class="thumbnail">
+            <img src="img/login.png" alt="Logg Inn" style="width:120%">
+            <div class="caption">
+            <p>Det første du må gjøre er å logge inn på accounten din. logg inn knappen finner du opp i høyre hjørne på nettsiden.</p>
+         </div>
+    </div> 
+</div>
+<div class="col-sm-4">
+        <div class="thumbnail">
+            <img src="img/registrerdeg.png" alt="registrerdeg" style="width:120%">
+            <div class="caption">
+            <p>Legg til tekst...</p>
+        </div>
+    </div>
+</div>
+<div class="col-sm-4">
+        <div class="thumbnail">
+            <img src="img/ferdigregistrert.png" alt="ferdigregistrert" style="width:120%">
+            <div class="caption">
+            <p>Legg til tekst...</p>
+        </div>
+    </div>
+</div>
+<div class="col-sm-4">
+        <div class="thumbnail">
+            <img src="img/nymappe.png" alt="nymappe" style="width:120%">
+            <div class="caption">
+            <p>Legg til tekst...</p>
+         </div>
+    </div> 
+</div>
+<div class="col-sm-4">
+        <div class="thumbnail">
+            <img src="img/navnmappe.png" alt="navnmappe" style="width:120%">
+            <div class="caption">
+            <p>Legg til tekst...</p>
+        </div>
+    </div>
+</div>
+<div class="col-sm-4">
+        <div class="thumbnail">
+            <img src="img/klikkmappa.png" alt="klikkmappa" style="width:120%">
+            <div class="caption">
+            <p>Legg til tekst...</p>
+        </div>
+    </div>
+</div>
+<div class="col-sm-4">
+        <div class="thumbnail">
+            <img src="img/nyknapper.png" alt="nyknapper" style="width:120%">
+            <div class="caption">
+            <p>Legg til tekst...</p>
+         </div>
+    </div> 
+</div>
+<div class="col-sm-4">
+        <div class="thumbnail">
+            <img src="img/leggtilimappa.png" alt="leggtilimappa" style="width:120%">
+            <div class="caption">
+            <p>Legg til tekst...</p>
+        </div>
+    </div>
+</div>
+<div class="col-sm-4">
+        <div class="thumbnail">
+            <img src="img/eksempelforlink.png" alt="eksempelforlink" style="width:120%">
+            <div class="caption">
+            <p>Legg til tekst...</p>
+        </div>
+    </div>
+</div>
+<div class="col-sm-4">
+        <div class="thumbnail">
+            <img src="img/lagttillink.png" alt="lagttillink" style="width:120%">
+            <div class="caption">
+            <p>Legg til tekst...</p>
+         </div>
+    </div> 
+</div>
+<div class="col-sm-4">
+        <div class="thumbnail">
+            <img src="img/logout.png" alt="logout" style="width:120%">
+            <div class="caption">
+            <p>Legg til tekst...</p>
+        </div>
+    </div>
+</div>
+</div>
+
+
+
 
 <?php
     require_once __DIR__ . '/include/footer.php';
