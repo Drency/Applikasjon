a9dd6fb46a7abbe7baf05dc5b54cca23a6f11e77
Nick <zick97@live.com>
2019-04-29 20:33:06 +0200

    Testdata
    Testdata for database

--- Database/Database.sql
+++ Database/Database.sql
@@ -56,3 +56,13 @@ INSERT INTO bibliotek(id)
 VALUES(new.id);
 END$$
 DELIMITER ;
+
+INSERT INTO brukere (brukernavn, email, passord)
+VALUES ('test', 'test@test.com', 'test'); 
+
+INSERT INTO mapper (mappeNavn, bibId)
+VALUES ('testmappe', '1');
+
+INSERT INTO links (linkNavn, linkUrl, mapId) 
+VALUES ('asdsa', 'afasfasd', '1');
+
